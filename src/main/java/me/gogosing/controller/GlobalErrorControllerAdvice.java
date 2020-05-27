3ad4a198f37baf342c0d0fc216692b3479003e4b
JinBum Jeong <gogosing@naver.com>
2020-05-27 12:56:49 +0900

    gradle build 시, 나타나는 warning 제거

--- src/main/java/me/gogosing/controller/GlobalErrorControllerAdvice.java
+++ src/main/java/me/gogosing/controller/GlobalErrorControllerAdvice.java
@@ -4,6 +4,7 @@ import me.gogosing.exception.NotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -18,18 +19,18 @@ public class GlobalErrorControllerAdvice extends ResponseEntityExceptionHandler 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<Object> notFoundHandler(NotFoundException exception, WebRequest request) {
         logger.error(exception.getMessage(), exception);
-        return new ResponseEntity(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @Override
     public ResponseEntity<Object> handleExceptionInternal(
             Exception exception,
-            Object body,
+            @Nullable Object body,
             HttpHeaders headers,
             HttpStatus status,
             WebRequest request
     ) {
         logger.error("handleExceptionInternal", exception);
-        return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
